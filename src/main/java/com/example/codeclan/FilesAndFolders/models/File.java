50de5813f35cc28eed8d99d16be724a9bcc7a970
glilley54 <glilley54@gmail.com>
2021-06-22 18:42:22 +0100

    first commit

---
+++ src/main/java/com/example/codeclan/FilesAndFolders/models/File.java
@@ -0,0 +1,4 @@
+package com.example.codeclan.FilesAndFolders.models;
+
+public class File {
+}
---
+++ src/main/java/com/example/codeclan/FilesAndFolders/models/Folder.java
@@ -0,0 +1,62 @@
+package com.example.codeclan.FilesAndFolders.models;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
+
+public class Folder implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "folder", fetch = FetchType.LAZY)
+    private List<File> files;
+
+    public Folder(Long id, String title, List<File> files) {
+        this.id = id;
+        this.title = title;
+        this.files = files;
+    }
+
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+}
+
+
+
